4fc123497b9fd4754ad869f2473e4890039f7cd7
Ofer Shaal <shaal@palantir.net>
2021-02-12 12:01:22 -0500

    Keep only minimal required files

--- web/sites/default/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// #ddev-generated: Automatically generated Drupal settings file.
-if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
-  include $app_root . '/' . $site_path . '/settings.ddev.php';
-}
